b1091858602604d4217f714cd7bf2d013f034be5
WHJWNAVY <whjwnavy@163.com>
2021-08-24 18:48:54 +0800

    optimization

--- demo/pair/run.sh
+++ demo/pair/run.sh
@@ -4,6 +4,6 @@ INSTALL_DIR=${INSTALL_PKGEXE_PATH}
 NNGPAIR_EXE=${INSTALL_DIR}/nng-pair
 
 ${NNGPAIR_EXE} node0 ipc:///tmp/pair.ipc & node0=$!
-${NNGPAIR_EXE}node1 ipc:///tmp/pair.ipc & node1=$!
+${NNGPAIR_EXE} node1 ipc:///tmp/pair.ipc & node1=$!
 sleep 3
 kill $node0 $node1
